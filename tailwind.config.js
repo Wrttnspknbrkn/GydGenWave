7a00366829fa0eb28a0264bf4504fcc2d0e4e151
Dodz.i <alexzormelo9@gmail.com>
2025-01-15 13:19:06 +0000

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary_color: "#2950DB",
-        primary_color_hover: "#2950db36",
-        secondary_color: "#7ca100",
-        text_primary: "#26272E",
-        text_secondary: "#171717",
-        text_light: "#797D7E",
-        border_light: "#f5f5f52f",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
