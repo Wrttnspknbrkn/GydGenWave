538db4354e125a1270d8627789dd58149a416986
takyifidelis <takyifidelis@gmail.com>
2025-01-15 14:17:53 +0000

    feat: material UI added successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
+
+
 function App() {
   return (
     <div className="flex justify-center items-center text-[4rem] font-[600] text-gray-500 h-screen">
-      Comming soon...
+     
+     <Button variant="contained">Hello world</Button>
+     <Switch {...label} disabled defaultChecked />
+     <Switch {...label} disabled />
     </div>
   );
 }
