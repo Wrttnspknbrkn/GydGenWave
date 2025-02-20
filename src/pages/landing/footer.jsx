60c18c32dbde8e91c57d8e0476d737a2d7bad669
Wrttnspknbrkn <kchampfameyeh@gmail.com>
2025-02-20 01:27:02 +0000

    Updated Footer content & its style

--- src/pages/landing/footer.jsx
+++ src/pages/landing/footer.jsx
@@ -2,102 +2,62 @@ import React from "react";
 import "./landing.scss";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer>
       <div className="newsletter">
         <div className="news">
           <h2>Join our newsletter</h2>
           <p>
-            Stay updated with the latest features <br /> and business tips from
-            GydGenWave.
+            Get the latest CRM insights, success stories, <br /> and product updates from
+            GydGenWave CRM.
           </p>
         </div>
         <div className="form">
-          <input type="email" placeholder="Email address" />
+          <input type="email" placeholder="Enter your business email" />
           <button>Subscribe</button>
         </div>
       </div>
       <hr />
       <div className="tabs flex">
         <div className="products">
-          <h3>Product</h3>
-
+          <h3>Features</h3>
           <ul>
-            <li>
-              <a href="">Inventory Management</a>
-            </li>
-            <li>
-              <a href="">Accounting & Fin. Management</a>
-            </li>
-            <li>
-              <a href="">HRM Module</a>
-            </li>
-            <li>
-              <a href="">Sales & Purchasing</a>
-            </li>
-            <li>
-              <a href="">Reporting & Analytics</a>
-            </li>
+            <li><a href="">Customer Management</a></li>
+            <li><a href="">Lead Management</a></li>
+            <li><a href="">Sales Pipeline</a></li>
+            <li><a href="">Marketing Automation</a></li>
+            <li><a href="">Analytics & Reporting</a></li>
           </ul>
         </div>
         <div className="community">
           <h3>Community</h3>
-
           <ul>
-            <li>
-              <a href="">Blog</a>
-            </li>
-            <li>
-              <a href="">Customer Stories</a>
-            </li>
-            <li>
-              <a href="">Upcoming Events</a>
-            </li>
-            <li>
-              <a href="">Forums</a>
-            </li>
+            <li><a href="">Success Stories</a></li>
+            <li><a href="">CRM Blog</a></li>
+            <li><a href="">Webinars</a></li>
+            <li><a href="">User Community</a></li>
           </ul>
         </div>
         <div className="learn">
-          <h3>Learn</h3>
-
+          <h3>Resources</h3>
           <ul>
-            <li>
-              <a href="">Documentation</a>
-            </li>
-            <li>
-              <a href="">Tutorials</a>
-            </li>
-            <li>
-              <a href="">FAQs</a>
-            </li>
-            <li>
-              <a href="">Setup Guide</a>
-            </li>
-            <li>
-              <a href="">Support Center</a>
-            </li>
+            <li><a href="">Getting Started Guide</a></li>
+            <li><a href="">Video Tutorials</a></li>
+            <li><a href="">Knowledge Base</a></li>
+            <li><a href="">API Documentation</a></li>
+            <li><a href="">Support Center</a></li>
           </ul>
         </div>
         <div className="cases">
-          <h3>Use Cases</h3>
-
+          <h3>Solutions</h3>
           <ul>
-            <li>
-              <a href="">For Small Businesses</a>
-            </li>
-            <li>
-              <a href="">Retail & Inventory Solutions</a>
-            </li>
-            <li>
-              <a href="">Finance & Accounting Automation</a>
-            </li>
-            <li>
-              <a href="">Customer Relationship Management</a>
-            </li>
-            <li>
-              <a href="">Sales & Project Management</a>
-            </li>
+            <li><a href="">Small Business CRM</a></li>
+            <li><a href="">Sales Teams</a></li>
+            <li><a href="">Marketing Teams</a></li>
+            <li><a href="">Customer Service</a></li>
+            <li><a href="">Remote Teams</a></li>
           </ul>
         </div>
       </div>
@@ -106,10 +66,10 @@ const Footer = () => {
         <div className="message">
           <div className="name flex">
             <div className="logo">
-              <img src="assets\images\company.svg" alt="" />
+              <img src="assets\images\company.svg" alt="GydGenWave CRM" />
             </div>
           </div>
-          <p className="copyright">© 2025 GydGenWave. All rights reserved.</p>
+          <p className="copyright">© {currentYear} GydGenWave CRM. All rights reserved.</p>
           <div className="address">
             <p>Address: 123 Business Hub, Accra, Ghana</p>
             <p>Phone: +233 123 456 789</p>
@@ -122,18 +82,18 @@ const Footer = () => {
           </div>
         </div>
         <div className="social flex">
-          <div className="media">
-            <img src="assets\images\facebook1.svg" alt="" />
-          </div>
-          <div className="media">
-            <img src="assets\images\twitter1.svg" alt="" />
-          </div>
-          <div className="media">
-            <img src="assets\images\instagram1.svg" alt="" />
-          </div>
-          <div className="media">
-            <img src="assets\images\linkedin1.svg" alt="" />
-          </div>
+          <a href="" className="media">
+            <img src="assets\images\facebook1.svg" alt="Facebook" />
+          </a>
+          <a href="" className="media">
+            <img src="assets\images\twitter1.svg" alt="Twitter" />
+          </a>
+          <a href="" className="media">
+            <img src="assets\images\instagram1.svg" alt="Instagram" />
+          </a>
+          <a href="" className="media">
+            <img src="assets\images\linkedin1.svg" alt="LinkedIn" />
+          </a>
         </div>
       </div>
     </footer>
