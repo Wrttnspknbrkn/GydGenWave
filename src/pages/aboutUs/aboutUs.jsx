dec1c551a8edce0d162f64a5272ca4b1f78d9cd5
Wrttnspknbrkn <kchampfameyeh@gmail.com>
2025-02-21 13:59:45 +0000

    Update aboutUs.jsx

--- src/pages/aboutUs/aboutUs.jsx
+++ src/pages/aboutUs/aboutUs.jsx
@@ -1,21 +1,14 @@
 import NavBar from "../landing/navbar";
 import Footer from "../landing/footer";
-import "../landing/landing.scss"; 
-import "./aboutUs.scss"; 
+import AboutSection from "./AboutSection";
+import "../landing/landing.scss";
+import "./aboutUs.scss";
 
 const AboutUs = () => {
   return (
     <div className="about-us-page">
       <NavBar />
-      
-      <div className="about-us-container">
-        {/* Placeholder for future content */}
-        <div className="about-us-header">
-          <h1>About Us</h1>
-          <p className="subtitle">Content coming soon</p>
-        </div>
-      </div>
-      
+      <AboutSection />
       <Footer />
     </div>
   );
